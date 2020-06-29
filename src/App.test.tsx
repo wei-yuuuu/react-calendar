f228e5c437c560b0bfb5a721c7a6c606f98b1842
weiyu <tony40508@gmail.com>
2020-06-29 23:02:39 +0800

    test: basic render testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders today in header', () => {
+  const { getByText } = render(<App />)
+  const now = new Date().toISOString().slice(0, 10)
+  expect(getByText(new RegExp(now, 'i'))).toBeInTheDocument()
+})
